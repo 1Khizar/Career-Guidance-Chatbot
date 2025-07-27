28cf6913ecb37a1573bdf9c2927c4405a269ee50
1Khizar <khizar12006@gmail.com>
2025-07-27 16:59:16 +0500

    Career Guidance Chatbot

--- app.py
+++ app.py
@@ -9,46 +9,75 @@ import nltk
 nltk.download('stopwords')
 
 from nltk.corpus import stopwords
+english_stopwords = stopwords.words('english')
+
+# Load the CSV saved from Jupyter
+df = pd.read_csv('CareerGuidanceDataset.csv')
 
 # Setup
 st.set_page_config(page_title="Career Guidance Chatbot", layout="centered")
 st.title('🎓 Career Guidance Chatbot')
 
+# Career Categories 
+career_roles = sorted(df['role'].unique())
+selected_role = st.selectbox("🎯 Choose a Career Role", career_roles)
 
-# Load the CSV saved from Jupyter
-df = pd.read_csv('CareerGuidanceDataset.csv')
-
-english_stopwords = stopwords.words('english')
-
+# load model
 model = joblib.load('chatbot_model.pkl')
+
+# load vectorizer
 vectorizer = joblib.load('vectorizer.pkl')
 
-
+# Function for cleaning the text / Preprocessing
 def clean_text(text):
     text = text.lower()
     for p in string.punctuation:
         text = text.replace(p, '')
     return ' '.join([word for word in text.split() if word not in english_stopwords ])
 
-df['cleaned_question'] = df['question'].apply(clean_text)
-dataset_ques = vectorizer.transform(df['cleaned_question'])
 
+# Form to ask the  question from the user
 with st.form("chat_form"):
     user_ques = st.text_input('Ask me a career-related question: ')
     submitted = st.form_submit_button("🔍 Get Answer")
 
+st.markdown(
+    """
+    <hr style="border: 1px solid #eaeaea; margin-top: 0px;">
+    <div style="text-align: center; font-size: 15px;">
+        👨‍💻 Developed with  by <b>Khizar Ishtiaq</b><br>
+        🔗 <a href="https://linkedin.com/in/khizar-ishtiaq-716518315" target="_blank">Visit my LinkedIn Profile</a>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+# Filter dataset for selected role
+filtered_df = df[df['role'] == selected_role].copy()
+
+# Clean the filtered questions
+filtered_df['cleaned_question'] = filtered_df['question'].apply(clean_text)
+
+# Vectorize the cleaned questions
+filtered_dataset_ques = vectorizer.transform(filtered_df['cleaned_question'])
+
+
+# Apply logic and give answer to user
 if submitted and user_ques:
+    
+    # Clean the user question 
     clean_que = clean_text(user_ques)
+    
+    # Convert the cleaned user question into vectors
     user_question_vec = vectorizer.transform([clean_que])
 
-    # Only need to transform once
-    similarity_scores = cosine_similarity(user_question_vec, dataset_ques)
+    # Finding the similarity between the user question and questions in dataset
+    similarity_scores = cosine_similarity(user_question_vec, filtered_dataset_ques)
 
     best_index = similarity_scores.argmax()
     best_score = similarity_scores[0, best_index]
 
-    if best_score > 0.6:
-        # print("Answer:", df['answer'][best_index])
+    if best_score > 0.3:
         st.success("Chatbot Answer: " + df['answer'][best_index])
     else:
         st.error("\nChatbot: Sorry, I don't have enough information about that.")
